07fbc35d1f2ef8eaf8a95bc403f5b4a1d848dda1
imjho <jholokar@wesleyan.edu>
2023-01-28 09:59:36 -0800

    update Login page

--- frontend/pages/Login.tsx
+++ frontend/pages/Login.tsx
@@ -18,10 +18,6 @@ function Login() {
     const email = target.email.value;
     const password = target.password.value;
 
-    // const email = (e.target.querySelector('#email') as HTMLInputElement).value;
-    // const password = (e.target.querySelector('#password') as HTMLInputElement)
-    //   .value;
-
     const info = { email, password };
 
     fetch('/api/auth/login', {
